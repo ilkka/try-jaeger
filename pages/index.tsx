b4a2099f623e0c830abc59e9afa29fb1eaaad3c3
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2020-01-22 11:25:10 +0100

    Make strict

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
-const Home = () => <h1>Hello world!</h1>;
+import { NextPage } from "next";
+
+interface HomeProps {
+  userAgent: string;
+}
+
+const Home: NextPage<HomeProps> = ({ userAgent }) => (
+  <h1>Hello world! — user agent: {userAgent}</h1>
+);
 
 Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers["user-agent"] : navigator.userAgent;
+  const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
   return { userAgent };
 };
 
